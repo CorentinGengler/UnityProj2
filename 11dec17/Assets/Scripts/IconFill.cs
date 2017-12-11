569324f0ad802c9877704b1f7eada3316e4b15b4
Technifutur <600@technifutur.be>
2017-12-11 15:09:07 +0100

    11dec17v4

---
+++ 11dec17/Assets/Scripts/IconFill.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class IconFill  : MonoBehaviour
+{
+
+    #region Public Members
+    public Image m_imageFiller;
+    public float m_filledAmount=0;
+
+    #endregion
+
+
+    #region Public Void
+    public void FillToAmount(float _Amount)
+    {
+        m_imageFiller.fillAmount = _Amount;
+    }
+    #endregion
+
+
+    #region System
+
+    void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
--- 11dec17/Assets/Scripts/MainButton.cs
+++ 11dec17/Assets/Scripts/MainButton.cs
@@ -6,22 +6,41 @@ public class MainButton  : MonoBehaviour
 {
 
     #region Public Members
-    public List<Sprite> listOfSprites = new List<Sprite>();
-    public List<string> UpperText = new List<string>();
-    public List<string> LowerText = new List<string>();
+    public List<Sprite> m_listOfSprites = new List<Sprite>();
+    public List<string> m_listOfUpperText = new List<string>();
+    public List<string> m_listOfLowerText = new List<string>();
+
+    public Image m_CanvasImage;
+    public Text m_CanvasUpperText;
+    public Text m_CanvasLowerText;
+
+
     #endregion
 
 
     #region Public Void
-
+    public void SwitchToNextImageAndText()
+    {
+        if(m_positionImage < (m_listOfSprites.Count)-1)
+        {
+            m_positionImage++;
+        }
+        else
+        {
+            m_positionImage = 0;
+        }
+        m_CanvasImage.sprite = m_listOfSprites[m_positionImage];
+        m_CanvasUpperText.text= m_listOfUpperText[m_positionImage];
+        m_CanvasLowerText.text= m_listOfLowerText[m_positionImage];
+}
     #endregion
 
 
     #region System
 
-    void Start () 
+    void Awake () 
     {
-		
+        m_positionImage = 0;
 	}
 	
 	void Update () 
@@ -41,7 +60,7 @@ public class MainButton  : MonoBehaviour
 
 
     #region Private And Protected Members
-
+    private int m_positionImage;
     #endregion
 
 }
