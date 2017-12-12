caeb7b9e4f19bb12237f7d87188adf2bdbd13187
Technifutur <600@technifutur.be>
2017-12-12 11:27:42 +0100

    ReignRemakeV1

---
+++ 11dec17/Assets/Scripts/Contenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contenu  : MonoBehaviour
+{
+
+    #region Public Members
+    public Sprite Image;
+    public string UpperText;
+
+    #endregion
+
+
+    #region Public Void
+
+    #endregion
+
+
+    #region System
+
+    void Start () 
+    {
+		
+	}
+    void Awake () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
---
+++ 11dec17/Assets/Scripts/ReignUIv1.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReignUIv1  : ReignsUI
+{
+
+    #region Public Members
+    
+    #endregion
+
+
+    #region Public Void
+
+    #endregion
+
+
+    #region System
+
+    void Start () 
+    {
+		
+	}
+    void Awake () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    public override void SetKingName(string kingName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetScoreTo(int score)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetYearOfReigns(int years)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetDialogue(string dialogue)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetImageName(string imageIdName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetAvatarImage(Sprite texture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusStatus(int index, bool isComplete)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusStatus(string questId, bool isComplete)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusIcon(int index, Sprite iconTexture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBonusIcon(string questId, Sprite iconTexture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetObjectiveStatus(int index, float pourcentageStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetObjectiveStatus(string objectiveId, float pourcentageStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetLeftAnswer(string text)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetRightAnswer(string text)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetContextBackground(string idToLoad)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetContextBackground(Sprite texture)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HighlighStatusPossiblity(int index, float pourcentageStatus, float nextPourcentStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HighlighStatusPossiblity(string objectiveName, float pourcentageStatus, float nextPourcentStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetAvatarName(string dialogue)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #endregion
+
+    #region Private Void
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
